4a486aa531ed12c31c27d0f7cc83de8ffd359dcd
crab999crab <837738663@qq.com>
2019-08-25 22:29:38 +0800

    这是用了vuex的todo小案例

--- src/store.js
+++ src/store.js
@@ -1,16 +1,50 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
-
+import axios from 'axios'
+import { log } from 'util';
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
+    state: {
+        list: [],
+        inputValue: 'abcdd',
+        nextID: 5
+    },
+    mutations: {
+        initList(state, data) {
+            state.list = data
+        },
+        setInputValue(state, val) {
+            state.inputValue = val
+        },
+        //添加列表项
+        addItem(state) {
+            const obj = {
+                id: state.nextID,
+                info: state.inputValue.trim(),
+                done: false
+            }
+            state.list.push(obj)
+            state.nextID++
+                state.inputValue = ''
+        },
+        //删除列表项
+        removeItem(state, id) {
+            //根据id查找对应项的索引
+            const i = state.list.findIndex(x => x.id === id)
+                //根据索引删除元素
+            if (i != -1) {
+                state.list.splice(i, 1)
+            }
+        }
 
-  },
-  mutations: {
-
-  },
-  actions: {
-
-  }
+    },
+    actions: {
+        getList(context) {
+            axios.get('/list.json').then(({ data }) => {
+                console.log(data);
+                context.commit('initList', data)
+            })
+        },
+    },
 })
